a7705f6f0c0a1d1933224c0b699c9dda15f58484
jmartinez729 <jmartinez729@gmail.com>
2019-06-03 18:12:22 -0400

    NavBar.js, Components added to front_end

---
+++ front_end/src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+
+  render() {
+    return (
+      <h1>NavBar</h1>
+    )
+  } // end of render()
+} // end of NavBar
+
+export default NavBar
